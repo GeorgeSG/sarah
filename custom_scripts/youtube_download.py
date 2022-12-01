89220b2bbdaa5a4ccefc3981a1180b044cc75eab
Georgi Gardev <georgi@gar.dev>
2022-12-01 16:01:59 +0200

    remove mentions on config folder

--- custom_scripts/youtube_download.py
+++ custom_scripts/youtube_download.py
@@ -8,7 +8,7 @@ youtube_url = sys.argv[1]
 out_name = sys.argv[2] if len(sys.argv) > 2 else 'temp'
 out_folder = sys.argv[3] if len(sys.argv) > 3 else ''
 
-full_file_path = f"""/config/www/audio/youtube/{out_folder}{out_name}"""
+full_file_path = f"""/www/audio/youtube/{out_folder}{out_name}"""
 
 ydl = youtube_dl.YoutubeDL({'format': 'bestaudio', 'outtmpl': full_file_path})
 
